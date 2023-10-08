28000a60e00ff95fa06b87187eb07094a158c6e2
jateq <temirlan.eraly1@gmail.com>
2023-10-08 17:21:58 +0600

    css changes tailwindcss

--- src/App.js
+++ src/App.js
@@ -7,6 +7,10 @@ function App() {
     <div>
       <Navbar/>
       <Landing/>
+
+        {/*<h1 className="text-3xl font-bold flex justify-center">*/}
+        {/*    Hello world!*/}
+        {/*</h1>*/}
     </div>
   );
 }

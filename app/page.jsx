b4243ad8982590d4fef7a4bee94aa699159f821f
jateq <temirlan.eraly1@gmail.com>
2023-10-10 23:45:45 +0600

    ui

---
+++ app/page.jsx
@@ -0,0 +1,11 @@
+import { getServerSession } from "next-auth/next";
+import { authOptions } from "@/lib/authOptions";
+import Landing from "@/components/home/landing";
+
+export default async function Home() {
+  const session = await getServerSession(authOptions);
+
+  return (
+      <Landing />
+  );
+}

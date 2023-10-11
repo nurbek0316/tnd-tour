ce00e66912c843db5c453b4aa863fd0f4c030fba
jateq <temirlan.eraly1@gmail.com>
2023-10-11 22:23:59 +0600

    auth google, scroll animation

--- components/home/landing.jsx
+++ components/home/landing.jsx
@@ -1,11 +1,21 @@
+'use client';
 
 import Balancer from "react-wrap-balancer";
+import { useState } from "react";
 import { DEPLOY_URL } from "@/lib/constants";
 
 export default function Landing() {
+  const [clicked, setClicked] = useState(false);
+
+  const handleClick = () => {
+    const screenHeight = window.innerHeight;
+    window.scrollTo({ top: screenHeight, behavior: 'smooth' });
+    setClicked(true);
+  };
+
   return (
     <>
-      <div className="z-10 w-full max-w-xl px-5 xl:px-0">
+      <div className="z-10 mt-[250px] w-full max-w-xl px-5 xl:px-0">
         <h1
           className="animate-fade-up bg-gradient-to-br from-black to-stone-500 bg-clip-text text-center font-display text-4xl font-bold tracking-[-0.02em] text-transparent opacity-0 drop-shadow-sm md:text-7xl md:leading-[5rem]"
           style={{ animationDelay: "0.15s", animationFillMode: "forwards" }}
@@ -21,12 +31,13 @@ export default function Landing() {
           </Balancer>
         </p>
         <div
-          className="mx-auto mt-6 flex animate-fade-up items-center justify-center space-x-5 opacity-0"
+          className="mx-auto mt-6  flex animate-fade-up items-center justify-center space-x-5 opacity-0"
           style={{ animationDelay: "0.3s", animationFillMode: "forwards" }}
+          onClick={handleClick}
         >
           <a
             className="group flex max-w-fit items-center justify-center space-x-2 rounded-full border border-black bg-black px-5 py-2 text-sm text-white transition-colors hover:bg-white hover:text-black"
-            href={"/"}
+
             rel="noopener noreferrer"
           >
             <svg
@@ -46,6 +57,7 @@ export default function Landing() {
             <p>Explore</p>
           </a>
         </div>
+        <div className="mb-[1000px]"></div>
       </div>
     </>
   )
